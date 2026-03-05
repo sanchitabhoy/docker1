8afab108d83fce39d83247c09f3fc7d7fd186d08
sanchitabhoy <sanchitabhoy@15948>
2026-03-05 13:43:20 +0530

    Run container with volume

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ app.get("/", (req, res)=>{
         [
             {
                 id: 1,
-                employee_name: "Sanchita",
+                employee_name: "Sanchita Bhoy",
                 employee_salary: 7000000,
             },
             {
                 id: 2,
-                employee_name: "John",
+                employee_name: "John Marty",
                 employee_salary: 600000,
             },
             {
                 id: 3,
-                employee_name: "Ruby",
+                employee_name: "Ruby D' Suza",
                 employee_salary: 80000,
             },
             {
                 id: 4,
-                employee_name: "Virat",
+                employee_name: "Virat Kohli",
                 employee_salary: 1500000,
             },
         ]
@@ -32,3 +32,4 @@ app.get("/", (req, res)=>{
 app.listen(8000, () =>{
     console.log("App is running on port number 8000")
 })
+
